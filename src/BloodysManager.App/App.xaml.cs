feaf81ab44dcae1321a2488626e0d1128b9dd3cc
Bloody2927 <Nick_Pisano@outlook.de>
2025-10-16 22:37:46 +0200

    Improve startup diagnostics and logging UX

--- src/BloodysManager.App/App.xaml.cs
+++ src/BloodysManager.App/App.xaml.cs
@@ -1,5 +1,47 @@
-namespace BloodysManager.App;
+using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
 
-public partial class App : System.Windows.Application
+namespace BloodysManager.App
 {
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Binding-Fehler sichtbar machen (Output-Fenster)
+            PresentationTraceSources.DataBindingSource.Switch.Level =
+                SourceLevels.Critical | SourceLevels.Error;
+
+            // Unhandled-Handler -> damit wir Hinweise bekommen statt „Silent Crash“
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            base.OnStartup(e);
+            try
+            {
+                var vm = new ViewModels.MainViewModel();
+                var win = new MainWindow { DataContext = vm };
+                MainWindow = win;
+                win.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Startup error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Shutdown(-1);
+            }
+        }
+
+        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.ToString(), "Unhandled UI exception", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true; // verhindert Sofort-Absturz, damit man Log/Output lesen kann
+        }
+
+        private void CurrentDomain_UnhandledException(object? sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ExceptionObject?.ToString() ?? "Unknown error", "Unhandled domain exception",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
 }
--- src/BloodysManager.App/MainWindow.xaml.cs
+++ src/BloodysManager.App/MainWindow.xaml.cs
@@ -1,52 +1,64 @@
 using System;
 using System.Windows;
+using System.Windows.Controls;
 using BloodysManager.App.ViewModels;
 
 namespace BloodysManager.App;
 
 public partial class MainWindow : Window
 {
-    private readonly MainViewModel _viewModel = new();
+    private MainViewModel ViewModel =>
+        DataContext as MainViewModel ?? throw new InvalidOperationException("MainViewModel is not set.");
 
     public MainWindow()
     {
         InitializeComponent();
-        DataContext = _viewModel;
     }
 
     protected override void OnClosed(EventArgs e)
     {
         base.OnClosed(e);
-        _viewModel.Dispose();
+        if (DataContext is MainViewModel vm)
+        {
+            vm.Dispose();
+        }
     }
 
-    private void SaveRepo_Click(object sender, RoutedEventArgs e) => _viewModel.SaveRepo();
-    private void SaveDownload_Click(object sender, RoutedEventArgs e) => _viewModel.SaveDownloadTarget();
+    private void LogBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (sender is TextBox tb)
+        {
+            tb.ScrollToEnd();
+        }
+    }
 
-    private void NewProfile_Click(object sender, RoutedEventArgs e) => _viewModel.NewProfile();
-    private void DeleteProfile_Click(object sender, RoutedEventArgs e) => _viewModel.DeleteProfile();
-    private void RenameProfile_Click(object sender, RoutedEventArgs e) => _viewModel.RenameProfile();
+    private void SaveRepo_Click(object sender, RoutedEventArgs e) => ViewModel.SaveRepo();
+    private void SaveDownload_Click(object sender, RoutedEventArgs e) => ViewModel.SaveDownloadTarget();
 
-    private void BrowseLive_Click(object sender, RoutedEventArgs e) => _viewModel.Browse(p => p.PathLive, (p, value) => p.PathLive = value);
-    private void BrowseCopy_Click(object sender, RoutedEventArgs e) => _viewModel.Browse(p => p.PathCopy, (p, value) => p.PathCopy = value);
-    private void BrowseBackup_Click(object sender, RoutedEventArgs e) => _viewModel.Browse(p => p.PathBackup, (p, value) => p.PathBackup = value);
-    private void BrowseBackupZip_Click(object sender, RoutedEventArgs e) => _viewModel.Browse(p => p.PathBackupZip, (p, value) => p.PathBackupZip = value);
+    private void NewProfile_Click(object sender, RoutedEventArgs e) => ViewModel.NewProfile();
+    private void DeleteProfile_Click(object sender, RoutedEventArgs e) => ViewModel.DeleteProfile();
+    private void RenameProfile_Click(object sender, RoutedEventArgs e) => ViewModel.RenameProfile();
 
-    private void CreateStructure_Click(object sender, RoutedEventArgs e) => _viewModel.CreateStructure();
-    private async void Download_Click(object sender, RoutedEventArgs e) => await _viewModel.DownloadAsync();
-    private async void Update_Click(object sender, RoutedEventArgs e) => await _viewModel.UpdateAsync();
-    private async void LiveCopy_Click(object sender, RoutedEventArgs e) => await _viewModel.LiveToCopyAsync();
-    private void DeleteLive_Click(object sender, RoutedEventArgs e) => _viewModel.DeleteLive();
-    private void DeleteCopy_Click(object sender, RoutedEventArgs e) => _viewModel.DeleteCopy();
-    private void Backup_Click(object sender, RoutedEventArgs e) => _viewModel.Backup();
-    private void Rotate_Click(object sender, RoutedEventArgs e) => _viewModel.RotateBackup();
+    private void BrowseLive_Click(object sender, RoutedEventArgs e) => ViewModel.Browse(p => p.PathLive, (p, value) => p.PathLive = value);
+    private void BrowseCopy_Click(object sender, RoutedEventArgs e) => ViewModel.Browse(p => p.PathCopy, (p, value) => p.PathCopy = value);
+    private void BrowseBackup_Click(object sender, RoutedEventArgs e) => ViewModel.Browse(p => p.PathBackup, (p, value) => p.PathBackup = value);
+    private void BrowseBackupZip_Click(object sender, RoutedEventArgs e) => ViewModel.Browse(p => p.PathBackupZip, (p, value) => p.PathBackupZip = value);
 
-    private void BrowseWorldExe_Click(object sender, RoutedEventArgs e) => _viewModel.BrowseWorldExe();
-    private void BrowseAuthExe_Click(object sender, RoutedEventArgs e) => _viewModel.BrowseAuthExe();
-    private void StartWorld_Click(object sender, RoutedEventArgs e) => _viewModel.StartWorld();
-    private void RestartWorld_Click(object sender, RoutedEventArgs e) => _viewModel.RestartWorld();
-    private void StopWorld_Click(object sender, RoutedEventArgs e) => _viewModel.StopWorld();
-    private void StartAuth_Click(object sender, RoutedEventArgs e) => _viewModel.StartAuth();
-    private void RestartAuth_Click(object sender, RoutedEventArgs e) => _viewModel.RestartAuth();
-    private void StopAuth_Click(object sender, RoutedEventArgs e) => _viewModel.StopAuth();
+    private void CreateStructure_Click(object sender, RoutedEventArgs e) => ViewModel.CreateStructure();
+    private async void Download_Click(object sender, RoutedEventArgs e) => await ViewModel.DownloadAsync();
+    private async void Update_Click(object sender, RoutedEventArgs e) => await ViewModel.UpdateAsync();
+    private async void LiveCopy_Click(object sender, RoutedEventArgs e) => await ViewModel.LiveToCopyAsync();
+    private void DeleteLive_Click(object sender, RoutedEventArgs e) => ViewModel.DeleteLive();
+    private void DeleteCopy_Click(object sender, RoutedEventArgs e) => ViewModel.DeleteCopy();
+    private void Backup_Click(object sender, RoutedEventArgs e) => ViewModel.Backup();
+    private void Rotate_Click(object sender, RoutedEventArgs e) => ViewModel.RotateBackup();
+
+    private void BrowseWorldExe_Click(object sender, RoutedEventArgs e) => ViewModel.BrowseWorldExe();
+    private void BrowseAuthExe_Click(object sender, RoutedEventArgs e) => ViewModel.BrowseAuthExe();
+    private void StartWorld_Click(object sender, RoutedEventArgs e) => ViewModel.StartWorld();
+    private void RestartWorld_Click(object sender, RoutedEventArgs e) => ViewModel.RestartWorld();
+    private void StopWorld_Click(object sender, RoutedEventArgs e) => ViewModel.StopWorld();
+    private void StartAuth_Click(object sender, RoutedEventArgs e) => ViewModel.StartAuth();
+    private void RestartAuth_Click(object sender, RoutedEventArgs e) => ViewModel.RestartAuth();
+    private void StopAuth_Click(object sender, RoutedEventArgs e) => ViewModel.StopAuth();
 }
